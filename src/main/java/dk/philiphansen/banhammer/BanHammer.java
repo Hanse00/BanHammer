e12e936bfb1ab92f6b3264ccfe00d0384dca7a3d
Philip Hansen <me@philiphansen.dk>
2014-03-22 15:56:13 +0100

    Initial mod creation

---
+++ src/main/java/dk/philiphansen/banhammer/BanHammer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 Philip Hansen.
+ *
+ * This file is part of BanHammer.
+ *
+ * BanHammer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BanHammer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BanHammer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.philiphansen.banhammer;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "banhammer", name = "Ban Hammer", version = "${version}")
+public class BanHammer {
+
+	@Instance("banhammer")
+	public static BanHammer instance;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {}
+}
